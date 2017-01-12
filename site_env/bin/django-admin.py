841ee0e78d2d3e4036c29d62829513c48d339fc2
Joseph Schueler <joehschueler@gmail.com>
2017-01-11 22:46:52 -0500

    add site envinonment
    Added site environments

---
+++ site_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/joseph/ExtraDrive1/Development/python_environments/workflo/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
